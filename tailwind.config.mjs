6f525bc0c9ece7fb3bc220f3cc87ec4515f955b4
brunston97 <brunston97@gmail.com>
2024-12-16 19:54:28 -0600

    attempted to add cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import nextui from '@nextui-org/react';
 
 export default {
   content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    nextui()
+  ]
 }
