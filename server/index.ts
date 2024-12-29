28cd7882843a3a97fea107072dee754cadc1c7d3
Mikal Mac <ventusboy13@gmail.com>
2024-12-29 01:57:16 -0500

    firestore db added

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import { FieldValue, Firestore } from '@google-cloud/firestore'
+
 const app = express()
 const PORT = parseInt(process.env.PORT ?? '8080')
 const openRouter = express.Router()
+
+const db = new Firestore({
+  projectId: 'license2plate-b4c6e',
+  keyFilename: process.env.KEY_FILE_PATH
+})
 
 openRouter.get('/', (req, res) => {
   //const name = process.env.NAME || 'World'
@@ -12,10 +19,31 @@ interface voteBody {
   id: string
 }
 
-openRouter.post('/vote/:id', (req, res) => {
+openRouter.post('/vote/:id', async (req, res) => {
   const { id } = req.params as voteBody
   console.log(`${id} has been voted`)
-  res.send('id')
+  const docRef = db.collection('plates').doc(id)
+  const { exists } = await docRef.get()
+  try {
+    if (!exists) {
+      await docRef.set({
+        voteCount: 0
+      })
+    }
+    await docRef.collection('votes').add({ time: Date.now() })
+    await docRef.update({
+      voteCount: FieldValue.increment(1)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  //const snapshot = await docRef.get()
+  // if (docRef.) {
+  //   await docRef.set(snapshot.data().id + 1)
+  // }
+
+  res.send(id)
 })
 app.use('/api', openRouter)
 
