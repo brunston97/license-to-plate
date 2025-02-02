0b645fe4b161130377bffd611c02473e52eeb695
brunston97 <brunston97@gmail.com>
2025-02-01 18:17:16 -0600

    i have no clue why this semicolon is needed but the site doesnt work without it

--- src/components/PlateOff.tsx
+++ src/components/PlateOff.tsx
@@ -90,7 +90,7 @@ const PlateOff = (props: PlateOffProps) => {
       const randomIndex = Math.floor(Math.random() * currentIndex)
       currentIndex--
 
-      [array[currentIndex], array[randomIndex]] = [
+      ;[array[currentIndex], array[randomIndex]] = [
         array[randomIndex],
         array[currentIndex]
       ]
