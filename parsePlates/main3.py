9f47fb92f2e4b48359ab72bf7b4a5d9e95598c59
mikal desktop <ventusboy13@gmail.com>
2025-12-13 12:54:54 -0500

    wip

--- parsePlates/main3.py
+++ parsePlates/main3.py
@@ -258,6 +258,23 @@ class LicensePlateProcess:
             return image_copy
         else :
             return None
+        
+    def compute_line_intersection(self, line1, line2) -> Optional[Tuple[int, int]]:
+        """
+        Finds intersection (x, y) of two lines given by ((x1, y1), (x2, y2)).
+        Uses determinants.
+        """
+        x1, y1, x2, y2 = line1
+        x3, y3, x4, y4 = line2
+
+        denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+        if denom == 0:
+            return None  # Parallel lines
+
+        px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom
+        py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom
+        
+        return int(px), int(py)
 
 
 if __name__ == "__main__":
